1f24b546a852069c361f04d6823889ac4bba8bad
Ben00n <benbahar321@gmail.com>
2025-05-03 17:16:18 +0300

    :)

--- GameServer/Infrastructure/Config/GameConfig.cs
+++ GameServer/Infrastructure/Config/GameConfig.cs
@@ -6,7 +6,6 @@
         public const int REVISION = 1;
         public const int MAX_PLAYERS = 5000;
 
-        // Database
         public const string CONNECTION_STRING = "Data Source=gamedb.sqlite";
     }
 }
